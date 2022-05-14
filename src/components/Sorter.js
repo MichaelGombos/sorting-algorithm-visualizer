797296c43fc45d7a52b1437774ae8b266f48be29
MichaelGombos <gombos2002@gmail.com>
2022-05-14 17:26:58 +0000

    Created Sorter Component file

---
+++ src/components/Sorter.js
@@ -0,0 +1,12 @@
+//This file is meant to hold the sorter component, aswell as the list of algorithms
+
+//the sorter function should instantly run, and then provide a "playback" object that allows the user to stop start...
+//and go backwards through the function
+
+import React from 'react'
+
+const Sorters(){
+    
+}
+
+export default Sorter
