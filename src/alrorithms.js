24a2001a7de064f2c6c929c0b0742d8fdd55cd3c
MichaelGombos <gombos2002@gmail.com>
2022-05-14 22:35:21 +0000

    Renamed "algorithm" key in sorter object to "steps from array" to better represent data

--- src/alrorithms.js
+++ src/alrorithms.js
@@ -5,35 +5,37 @@ const logStep = (currentRay,index1,index2) => {
      change: `Swapped ${currentRay[index1]} at ray[${index1}] with ${currentRay[index2]} at ray[${index2}]`,
      currentArray: [...currentRay],
      percentDone:null
-     }
+     } 
      return step;
 }
 
+//its important that the algorims do not mutate the original array.
 const bubbleSort = {
     name:"bubble sort",
     description:"//todo informative description of the bubble sort algorithm",
-    algorithm:  (ray) =>
+    stepsFromArray:  (ray) =>
 {
   //hold an array of step objects, this will allow us to "backtrack" this sort later.
   let StepArray = [];
+  let currentRay = [...ray]
   let n = ray.length;
   let i, j;
   for (i = 0; i < n-1; i++)
   {
       for (j = 0; j < n-i-1; j++)
       {
-          if (ray[j] > ray[j+1])
+          if (currentRay[j] > currentRay[j+1])
           {
           //swap
-          let temp = ray[j];
-          ray[j] = ray[j+1];
-          ray[j+1] = temp;
+          let temp = currentRay[j];
+          currentRay[j] = currentRay[j+1];
+          currentRay[j+1] = temp;
           
           //add new step object to step array
           // console.log(`Swapped ${ray[j]} at ray[${j}] with ${ray[j+1]} at ray[${j+1}]`)
           // console.log(`CurrentRay ${ray}`);
            
-           StepArray.push(logStep(ray,j,j+1));
+           StepArray.push(logStep(currentRay,j,j+1));
            //step ray append current
           }
       }
@@ -50,12 +52,13 @@ const bubbleSort = {
 const selectionSort = {
     name:"selection sort",
     description:"//todo informative description of the Selection sort algorithm",
-    algorithm: (ray) => 
+    stepsFromArray: (ray) => 
     {
         let StepArray = [];
         console.log("iran2")
         let n = ray.length;
         var i, j, min_idx;
+        let currentRay = [...ray];
      
         // One by one move boundary of unsorted subarray
         for (i = 0; i < n-1; i++)
@@ -63,17 +66,17 @@ const selectionSort = {
             // Find the minimum element in unsorted array
             min_idx = i;
             for (j = i + 1; j < n; j++)
-                if (ray[j] < ray[min_idx])
+                if (currentRay[j] < currentRay[min_idx])
                     min_idx = j;
          
                 // Swap the found minimum element with the first element
-                    var temp = ray[min_idx];
-                    ray[min_idx] = ray[i];
-                    ray[i] = temp;
+                    var temp = currentRay[min_idx];
+                    currentRay[min_idx] = currentRay[i];
+                    currentRay[i] = temp;
                     
-                    StepArray.push(logStep(ray,i,min_idx));
+                    StepArray.push(logStep(currentRay,i,min_idx));
         }
-        return ray;
+        return currentRay;
     }
 }
 
